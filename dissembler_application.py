888405d823a8c655029cf54ae3c8815c422d8ff0
Kevin Huang <kehuang8@gmail.com>
2018-12-17 21:51:01 -0500

    Added create_widgets() method

--- dissembler_application.py
+++ dissembler_application.py
@@ -4,26 +4,44 @@ class Application:
 	'''
 	Represents the main application window of a dissembler game.
 	'''
-	def __init__(self, master):
+	def __init__(self, master, init_size=(800,600)):
 		'''
 		Arguments:
 			master: a tk.Tk object that manages the window
+			init_size: a tuple of two non-negative integers representing the
+			initial size of the game window (width, height) in pixels
 
 		Creates and populates all of the GUI elements of the game.
 		'''
 		self.master = master
+		self.width, self.height = init_size
+		self.master.geometry(f'{self.width}x{self.height}')
 
 		self.create_widgets()
-		self.populate_widgets()
 
 	def mainloop(self):
 		self.master.mainloop()
 
 	def create_widgets(self):
-		pass
+		'''
+		Creates all of the widgets in the GUI. Consists of 3 main widgets:
 
-	def populate_widgets(self):
-		pass
+		- self.menu_canvas: the canvas which holds the drop-down menu
+		- self.game_canvas: the canvas which holds the actual dissembler game
+		- self.info_frame: a frame that contains text displayed to the player
+		'''
+		self.menu_canvas = tk.Canvas(self.master, width=self.width, 
+			height=self.height/6)
+
+		self.game_canvas = tk.Canvas(self.master, width=self.width, 
+			height=self.height/2)
+
+		self.info_frame = tk.Frame(self.master, width=self.width, 
+			height=self.height/3)
+
+		self.menu_canvas.pack(fill='both')
+		self.game_canvas.pack(fill='both')
+		self.info_frame.pack(fill='both')
 
 	def key_handler(self):
 		pass
